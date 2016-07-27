10820c452e5ae357874ed93e63cd27a5bad5a2e5
Rodrigo Silva Rahman de Almeida <dgrodrigo@gmail.com>
2016-07-27 19:44:43 -0300

    Incrementando versao

---
+++ README.md
@@ -0,0 +1,7 @@
+## Acesso Dropbox com Python##
+
+***Registrar a APP no developers.dropbox.com***
+
+***Gerar o OAuth2***
+
+Substituir o oauth_key com essa chave
--- dropbox.py
+++ dropbox.py
@@ -1,7 +1,9 @@
 import dropbox
 
-client = dropbox.client.DropboxClient("ge5n4SXKJ9gAAAAAAAASeyi5ntOgIjkWErD4wWkoAHuzcpnz5Rwh82C80Mf9c3ZA")
-print 'linked account: ', client.account_info()
+oauth_key = "ge5n4SXKJ9gAAAAAAAASeyi5ntOgIjkWErD4wWkoAHuzcpnz5Rwh82C80Mf9c3ZA"
+
+client = dropbox.client.DropboxClient(oauth_key)
 
 f = open('pacotes.txt')
+
 client.put_file('pacotes.txt', f);
